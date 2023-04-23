082338fa04cb787e1b6c8ded94704d88f8569d7b
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-04-23 18:27:04 +0545

    feat:navigation routes in react

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../component/Header";
+export const About = () => {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>About</h1>
+      </section>
+    </>
+  );
+};
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Header } from "../component/Header";
+export const Contact = () => {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>Contact</h1>
+      </section>
+    </>
+  );
+};
