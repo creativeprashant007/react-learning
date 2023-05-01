39b31a2be65fe8b126452811ea5059414bceebf0
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-05-01 15:13:11 +0545

    fix:error page and navigation items

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,22 +1,52 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import imageLogo from "../assets/images/logo.png";
 
 export const Header = () => {
   return (
-    <header>
-      <NavLink to="#">LOGO</NavLink>
+    <header
+      style={{
+        paddingLeft: 50,
+        paddingTop: 10,
+        background: "linear-gradient(to right, #1abc9c, #3498db)",
+        color: "black",
+      }}
+    >
+      <NavLink to="#">
+        <img
+          style={{
+            borderRadius: 20,
+            boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.25)",
+          }}
+          src={imageLogo}
+          width="60"
+          height="60"
+        />
+      </NavLink>
       <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
+        <div>
+          <ul
+            style={{
+              flexDirection: "row",
+              flex: 1,
+              width: "30%",
+              fontSize: 20,
+              display: "inline-flex",
+              justifyContent: "space-between",
+              listStyle: "none",
+            }}
+          >
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export const ErrorPage = () => {
-  return <div>404 ERROR</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        textAlign: "center",
+        paddingTop: "50vh",
+        height: "100vh",
+        color: "#fff",
+        fontSize: 80,
+        width: "100vw",
+      }}
+    >
+      404 ERROR
+    </div>
+  );
 };
