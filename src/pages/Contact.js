1c66430b894f63c3b53e1f4191cb4dbc890adbde
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-06-01 20:23:44 +0545

    feat:breadcumb and form component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,87 @@
 import React from "react";
+import { Button, Form, Input, Breadcrumb } from "antd";
 
 import { Header } from "../component/Header";
 export const Contact = () => {
+  const { TextArea } = Input;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <>
       <Header />
-      <section>
-        <h1>Contact</h1>
-      </section>
+      <div style={{ margin: "30px" }}>
+        <Breadcrumb
+          items={[
+            {
+              title: "Home",
+            },
+
+            {
+              title: "Contact",
+            },
+          ]}
+        />
+        <Form
+          style={{ marginTop: "30px" }}
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Message"
+            name="message"
+            rules={[
+              {
+                required: true,
+                message: "Please input your message!",
+              },
+            ]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </>
   );
 };
